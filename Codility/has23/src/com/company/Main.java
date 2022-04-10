2227b9fc326ed0f2368897a90aa02829eb285d27
susztek.norbert <susztek.norbert@gmail.com>
2022-04-10 14:50:14 +0200

    Codility

---
+++ Codility/has23/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] nums = {1,2};
+    }
+    public boolean has23(int[] nums) {
+
+        if(nums[0]==2 || nums[1]==2 || nums[0]==3 || nums[1]==3){
+            return true;
+        }
+            return false;
+    }
+
+}
